54a2483274f67b4c703e4c97facfb09b3a60510b
desirel <two_somang@icloud.com>
2021-12-19 22:56:56 +0900

    Default Layout Setting

---
+++ TodoApp/TodoCell.swift
@@ -0,0 +1,20 @@
+//
+// Created by Desire L on 2021/12/19.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    @IBOutlet
+    var topTitleLabel: UILabel!
+
+    @IBOutlet
+    var priorityView: UIView!
+
+
+    @IBOutlet
+    var dateLabel: UILabel!
+
+
+}
--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -11,10 +11,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet
+    var todoTableView: UITableView!
+
+
     override
     func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "TODO App"
+        makeNavigationBar()
+
+        todoTableView.delegate = self
+        todoTableView.dataSource = self
+    }
+
+    func makeNavigationBar() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.prefersLargeTitles = true
+
+        let item = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTodo))
+        item.tintColor = .black.withAlphaComponent(0.7)
+        navigationItem.rightBarButtonItem = item
+
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.backgroundColor = .blue.withAlphaComponent(0.2)
+
+        /**
+         - Note: Navigation 바 영역의 색상을 변환
+         */
+        navigationBar.standardAppearance = barAppearance
+        navigationBar.scrollEdgeAppearance = barAppearance
+    }
+
+
+    @objc
+    func addTodo() {
+
+    }
+
+}
+
+
+// ### EXTENSION ### //
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
     }
 
 
